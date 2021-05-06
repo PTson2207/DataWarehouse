707e6a5f92245fdfeaca80e313c48900474631af
Master-Snack <sonphukl@gmail.com>
2021-05-06 19:05:45 +0700

    Initilaze SQL query to datawarehouse

--- sql_queries.py
+++ sql_queries.py
@@ -3,6 +3,16 @@ import configparser
 config = configparser.ConfigParser()
 config.read("datawarehouse.conf")
 
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABle IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE  IF EXISTS time;"
+
 # create tables 
 staging_events_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_envents 
@@ -28,6 +38,22 @@ CREATE TABLE IF NOT EXISTS staging_envents
 );
 """)
 
+staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs
+(
+    num_songs INTEGER,
+    artist_id VARCHAR,
+    artist_latitude FLOAT,
+    artist_longitude FLOAT,
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    song_id VARCHAR,
+    title VARCHAR,
+    duration FLOAT,
+    year FLOAT
+);
+""")
+
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays
     (
@@ -43,5 +69,133 @@ songplay_table_create = ("""
     )
     DISTSTYLE KEY
     DISTKEY (start_time)
-    SORTKEY (S)
+    SORTKEY (S);
 """)
+
+user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs
+    (
+        song_id VARCHAR PRIMARY KEY,
+        title VARCHAR,
+        artist_id VARCHAR,
+        year INTEGER ENCODE BYTEDICT,
+        duration FLOAT
+    )
+    SORTKEY (song_id);
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs
+(
+    song_id VARCHAR PRIMARY KEY,
+    title VARCHAR,
+    artist_id VARCHAR,
+    year INTEGER ENCODE BYTEDICT,
+    duration FLOAT
+)
+SORTKEY (song_id);
+""")
+
+artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists
+    (
+        artist_id VARCHAR PRIMARY_KEY,
+        name VARCHAR,
+        location VARCHAR,
+        latitude FLOAT,
+        longtitude FLOAT
+    )
+    SORTKEY (artist_id);
+""")
+
+time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time 
+    (
+    start_time TIMESTAMP PRIMARY KEY,
+    hour INTEGER,
+    day INTEGER,
+    week INTEGER,
+    month INTEGER,
+    year INTEGER ENCODE BYTEDICT,
+    weekdat VARCHAR(9) ENCODE BYTEDICT
+    )
+    DISTSTYLE KEY
+    DISTKEY (start_time)
+    SORTKEY (start_time);
+""")
+
+staging_events_copy = ("""
+COPY staging_events
+FROM {}
+iam_role {}
+FORMAT AS json {};
+""").format(config['S3']['LOG_DATA'], config['iam_role']['arn'], config['S3']['LOG_JSONPATH'])
+
+staging_songs_copy = ("""
+COPY staging_songs
+FROM {}
+iam_role {}
+FORMAT AS json 'auto';
+""").format(config['S3']['SONG_DATA'], config['iam_role']['arn'])
+
+# final table
+songplay_table_insert = ("""
+    INSERT INTO songplays (START_TIME, USER_ID, LEVEL, SONG_ID, ARTIST_ID, SESSION_ID, LOCATION, USER_AGENT)
+    SELECT DISTINCT
+            TIMESTAMP 'epoch' + (se.ts / 1000) * INTERVAL '1 second' as start_time,
+            se.userId, 
+            se.level,
+            ss.song_id,
+            ss.artist_id,
+            se.location,
+            se.sessionId,
+            se.userAgent
+    FROM staging_songs ss
+    INNER JOIN staging_envents se
+    ON (ss.title = se.song AND se.artist = ss.artist_name)
+    AND se,page = 'NextSong';
+""")
+
+user_table_insert = ("""
+    INSERT INTO users
+    SELECT DISTINCT userId, userName, lastName, gendder, level
+    FROM staging_events
+    WHERE userId IS NOT NULL
+    AND page = 'NextSong';
+""")
+
+song_table_insert = ("""
+    INSERT INTO songs
+    SELECT 
+        DISTINCT song_id, title, artist_id, year, duration
+    FROM staging_songs
+    WHERE song_id is NOT_NULL;
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists
+SELECT
+    DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+FROM staging_songs;
+""")
+
+time_table_insert = ("""
+insert into time
+SELECT DISTINCT
+       TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second' as start_time,
+       EXTRACT(HOUR FROM start_time) AS hour,
+       EXTRACT(DAY FROM start_time) AS day,
+       EXTRACT(WEEKS FROM start_time) AS week,
+       EXTRACT(MONTH FROM start_time) AS month,
+       EXTRACT(YEAR FROM start_time) AS year,
+       to_char(start_time, 'Day') AS weekday
+FROM staging_events;
+""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+
