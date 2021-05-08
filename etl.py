1c77893f3d8f433201b7a5f6ed65429e614e51bd
Master-Snack <sonphukl@gmail.com>
2021-05-08 17:13:02 +0700

    create DAG in ETL

--- etl.py
+++ etl.py
@@ -1,3 +1,25 @@
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
+
+def load_staging_tabels(cur, conn):
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def main():
+    parser = configparser.ConfigParser()
+    parser.read("datawarehouse.conf")
+
+    conn = psycopg2.connect("host= {} dbname={} user={} password={} port={}".format(*parser['cluster'].values()))
+    cur = conn.cursor()
+
+    load_staging_tabels(cur, conn)
+    insert_tables(conn, cur)
+
+    conn.close()
